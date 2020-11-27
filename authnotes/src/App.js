0e962bac99faa22c408682fce3994f77fe5009b9
Shivansh Srivastava <shivanshsrivastava2000@gmail.com>
2020-11-27 11:22:31 +0530

    Basic Notes Added

--- authnotes/src/App.js
+++ authnotes/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Note from'./Note/Note';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      notes : [
+        {id:1, noteContent:'First Note'},
+        {id:2, noteContent:'Second Note'}
+      ]
+    }
+  }
+    render(){
+    return (
+      <div className="App">
+        {
+        this.state.notes.map((note) => {
+          return (
+            <Note 
+            noteContent = {note.noteContent}
+            id = {note.id}
+            key = {note.id} />
+          )
+        })
+        }
+      </div>
+    );
+  }
+}
 export default App;
---
+++ authnotes/src/Note/Note.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Note = (props) => {
+    return (
+        <div className="singleNote">
+            {props.noteContent}
+        </div>
+    )
+}
+
+
+Note.propTypes = {
+    noteContent: PropTypes.string
+}
+
+export default Note;
